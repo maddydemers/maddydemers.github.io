511805199d32bc78643a517a08f4fcc21d702962
Madeline Demers <demersmaddy@gmail.com>
2024-04-06 20:55:23 -0400

    polishing

--- public/scripts/map.js
+++ public/scripts/map.js
@@ -1,4 +1,3 @@
-// Function to load JSON data from file
 async function loadData() {
     try {
         const response = await fetch('private/strava.json');
@@ -11,26 +10,21 @@ async function loadData() {
     }
 }
 
-// Initialize Leaflet map
+// Source: Leaflet
 var map = L.map('map').setView([42.3736, -71.1097], 3);
 
-// Add OpenStreetMap tiles
+// Source: Leaflet/OpenStreetMap
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-// Call loadData function to fetch JSON data
 loadData().then(data => {
-    // Ensure that data.start_latlng exists and has a length property
     if (data && data.start_latlng) {
-        // Create an empty array to store markers
         var markers = [];
 
-        // Loop through the data
         for (var key in data.start_latlng) {
             if (data.start_latlng.hasOwnProperty(key)) {
-                // Parse latitude and longitude from string
                 var latLngString = data.start_latlng[key];
                 var latLngArray = JSON.parse(latLngString);
                 var lat = latLngArray[0];
@@ -45,16 +39,16 @@ loadData().then(data => {
                 dist = parseFloat(dist).toFixed(2); 
                 vert = parseFloat(dist).toFixed(0); 
 
-                // Create marker and bind popup
+                // .bindPopup("<b>" + type + "</b><br>Date: " + month+ "/" + year+ "<br>Distance: " + dist + " mi" +"<br>Elevation Gain: " + vert + " ft");
+
                 var marker = L.marker(latlng)
                     .addTo(map)
                     .bindPopup("<b>" + type)
-                    .bindPopup("<b>" + type + "</b><br>Date: " + month+ "/" + year+ "<br>Distance: " + dist + " mi" +"<br>Elevation Gain: " + vert + " ft");
+                    .bindPopup("<b>" + type + "</b><br>Distance: " + dist + " mi" +"<br>Elevation Gain: " + vert + " ft");
                     marker.on('click', function(e) {
                         map.setView(e.latlng, 10); 
                     });
 
-                // Add marker to markers array
                 markers.push(marker);
                 console.log("Number of markers added:", markers.length);
             }
